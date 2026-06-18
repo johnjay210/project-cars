708b199958f519f955769c9e776e92dfecef384b
johnjay210 <ngangajohnson210@gmail.com>
2026-06-18 12:38:48 +0300

    Add location fields and index to cars table

--- autohavencars/database/add_location_fields.sql
+++ autohavencars/database/add_location_fields.sql
@@ -1,9 +1,8 @@
 -- Add Location Fields to Cars Table
--- Run this SQL in phpMyAdmin or MySQL command line
 
 USE autohavencars;
 
--- Add location columns to cars table
+-- AddING location columns to cars table
 ALTER TABLE cars 
 ADD COLUMN IF NOT EXISTS city VARCHAR(100) AFTER transmission,
 ADD COLUMN IF NOT EXISTS state VARCHAR(50) AFTER city,
@@ -16,16 +15,7 @@ ADD COLUMN IF NOT EXISTS longitude DECIMAL(11, 8) AFTER latitude;
 ALTER TABLE cars 
 ADD INDEX IF NOT EXISTS idx_location (city, state);
 
--- For MySQL versions that don't support IF NOT EXISTS, use this instead:
--- ALTER TABLE cars 
--- ADD COLUMN city VARCHAR(100) AFTER transmission,
--- ADD COLUMN state VARCHAR(50) AFTER city,
--- ADD COLUMN zip_code VARCHAR(20) AFTER state,
--- ADD COLUMN address VARCHAR(255) AFTER zip_code,
--- ADD COLUMN latitude DECIMAL(10, 8) AFTER address,
--- ADD COLUMN longitude DECIMAL(11, 8) AFTER latitude;
--- 
--- ALTER TABLE cars ADD INDEX idx_location (city, state);
+
 
 
 
